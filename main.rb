12e7806d1bd2aa4efeba9617d9e6f246ce2960d0
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-08-13 23:24:04 -0400

    Add player and board generation

--- main.rb
+++ main.rb
@@ -1,10 +1,28 @@
+require colorize
 class Player
-  attr_accessor :player_id
-  @player_id = 0
-  
+  attr_accessor :player_id, :player_mark, :player_points, :number_of_moves
+  self.player_id = 0
+
+  def initiailize 
+    self.player_id += 1
+    player_id == 1 ? self.player_mark = "X" : self.player_mark = "O" 
+    self.player_points = 0
+    self.number_of_moves = 0
+  end
 end
 
 class Board
+  def board 
+    current = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    3.times do |y|
+      3.times { |x| print '%3i' % current[x][y] }
+      puts
+    end
+  end
+
+  def board_update
+    
+  end
 end
 
 class Game
