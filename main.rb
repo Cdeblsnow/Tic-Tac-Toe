63af183481d06c838d7faa50b9a5f8a4896dbf90
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-08-14 00:36:17 -0400

    Fix board error and add Gemfile

--- main.rb
+++ main.rb
@@ -1,10 +1,10 @@
-require colorize
+require 'colorize'
 class Player
   attr_accessor :player_id, :player_mark, :player_points, :number_of_moves
-  self.player_id = 0
+  @@player_id = 0
 
   def initiailize 
-    self.player_id += 1
+    @@player_id += 1
     player_id == 1 ? self.player_mark = "X" : self.player_mark = "O" 
     self.player_points = 0
     self.number_of_moves = 0
@@ -12,18 +12,34 @@ class Player
 end
 
 class Board
-  def board 
-    current = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+  @@current_board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+  
+  def initial_board 
     3.times do |y|
-      3.times { |x| print '%3i' % current[x][y] }
+      3.times { |x| print '%3i' % @@current_board[x][y] }
       puts
     end
   end
 
-  def board_update
-    
+  def finding
+    board_index = 0
+    3.times do |y|
+      3.times do |x|
+        if @@current_board[x][y] == 2 #check for the index of the object if valid
+          board_index = "#{x}#{y}"
+          p board_index
+          break
+        end
+      end
+    end
   end
+
 end
 
 class Game
 end
+
+c = Board.new
+c.initial_board
+c.finding
