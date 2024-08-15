cbaccc7ea29915e9e627555a2f18c94a363c27f5
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-08-15 00:41:36 -0400

    Remove unused class and add board/player logic

--- main.rb
+++ main.rb
@@ -1,45 +1,76 @@
-require 'colorize'
+require "colorize"
+require "rubocop"
 class Player
-  attr_accessor :player_id, :player_mark, :player_points, :number_of_moves
+  attr_accessor :player_mark, :player_points, :number_of_moves
+
   @@player_id = 0
 
-  def initiailize 
+  def initialize
     @@player_id += 1
-    player_id == 1 ? self.player_mark = "X" : self.player_mark = "O" 
+    self.player_mark = (@@player_id == 1 ? "X" : "O")
     self.player_points = 0
     self.number_of_moves = 0
+  end
+
+  def show_player_id
+    @@player_id
+  end
+
+  def add_move
+    self.number_of_moves += 1
+  end
+
+  def add_point
+    self.player_points += 1
   end
 end
 
 class Board
+  attr_accessor :posx, :posy, :new_board
 
   @@current_board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-  
-  def initial_board 
+
+  def initialize
+    self.posx = 0
+    self.posy = 0
+  end
+
+  def show_board
     3.times do |y|
-      3.times { |x| print '%3i' % @@current_board[x][y] }
+      3.times { |x| print "%3i" % @@current_board[x][y] } # 'vertical printing'
       puts
     end
   end
 
-  def finding
-    board_index = 0
+  def finding(move)
     3.times do |y|
       3.times do |x|
-        if @@current_board[x][y] == 2 #check for the index of the object if valid
-          board_index = "#{x}#{y}"
-          p board_index
-          break
-        end
+        next unless @@current_board[x][y] == move # check for the index of the object if valid
+
+        self.posx = x
+        self.posy = y
+        p posx
+        p posy
+        break
       end
     end
   end
 
+  def transformable_board
+    self.new_board = @@current_board
+  end
+
+  def show_pos
+    x = posx
+    y = posy
+    p new_board[x][y]
+  end
 end
 
-class Game
-end
+boo = Board.new
+player1 = Player.new
+player2 = Player.new
 
-c = Board.new
-c.initial_board
-c.finding
+boo.finding(2)
+boo.transformable_board
+boo.show_pos
