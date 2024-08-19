87823a65408cf6da922668f369a6f3d138d10029
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-08-18 23:18:39 -0400

    Fix bugs

--- main.rb
+++ main.rb
@@ -9,7 +9,7 @@ class Player
   def initialize
     @@player_id += 1
     self.name = "Player#{@@player_id}"
-    self.player_mark = (@@player_id == 1 ? "X" : "O")
+    self.player_mark = (@@player_id == 1 ? "X".colorize(:red) : "O".colorize(:blue))
     self.player_points = 0
   end
 
@@ -34,27 +34,26 @@ class Board
 
   def show_board
     3.times do |y|
-      3.times { |x| print "%3i" % @@current_board[x][y] } # 'vertical printing'
+      3.times { |x| print new_board[x][y] } # 'vertical printing'
       puts
     end
   end
 
   def finding(move)
-    3.times do |y|
-      3.times do |x|
-        next unless @@current_board[x][y] == move # check for the index of the object if valid
+    p move
+    3.times do |x|
+      3.times do |y|
+        next unless new_board[x][y] == move # check for the index of the object if valid
 
         self.posx = x
         self.posy = y
-        p posx
-        p posy
-        break
+        return
       end
     end
   end
 
   def reset_board
-    self.new_board = @@current_board # reset and create the board we will be working with
+    self.new_board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # reset and create the board we will be working with
   end
 
   def transform_board(mark)
@@ -67,13 +66,13 @@ class Board
 
   def show_transformed_board
     3.times do |y|
-      3.times { |x| print "%3i" % new_board[x][y] } # 'vertical printing'
+      3.times { |x| print new_board[x][y] } # 'vertical printing'
       puts
     end
   end
 
   def horizontal_win?
-    new_board.each do |row|
+    3.times do |row|
       2.times do |i|
         break unless new_board[row][i] == new_board[row][i + 1]
         return true if i == 1
@@ -82,7 +81,7 @@ class Board
   end
 
   def vertical_win?
-    new_board.each do |column|
+    3.times do |column|
       2.times do |i|
         break unless new_board[i][column] == new_board[i + 1][column]
         return true if i == 1
@@ -106,6 +105,7 @@ class Board
 end
 
 board = Board.new
+board.reset_board
 player1 = Player.new
 player2 = Player.new
 winner = false
@@ -114,28 +114,24 @@ playing_order = playing_order.shuffle
 firstp = playing_order[0]
 secondp = playing_order[1]
 
-puts "#{firstp.name} goes first their mark is" + " #{firstp.player_mark}".colorize(:red)
-puts "#{secondp.name} goes first their mark is" + " #{secondp.player_mark}".colorize(:blue)
+puts "#{firstp.name} goes first their mark is" + " #{firstp.player_mark}"
+puts "#{secondp.name} goes first their mark is" + " #{secondp.player_mark}"
 puts ""
 puts board.show_board
 
 until winner == true
 
   puts "#{firstp.name} make your move"
-  move = gets.chomp
-  board.transform_board(move)
-  unless board.transform_board(move) == true
-    puts "Please try again"
-    move = gets.chomp
-    board.transform_board(move)
-  end
+  move = gets.chomp.to_i
+  board.finding(move)
+  board.transform_board(firstp.player_mark)
   board.show_transformed_board
 
   if board.horizontal_win? == true || board.vertical_win? == true ||
      board.down_left_diagonal_win? == true || board.down_right_diagonal_win?
 
     firstp.add_point
-    puts "#{firstp} congratulations for wining!"
+    puts "#{firstp.name} congratulations for wining!"
     winner = true
     puts "Would you like to keep playing Y/N?"
     answer = gets.chomp
@@ -146,31 +142,27 @@ until winner == true
       firstp = playing_order[0]
       secondp = playing_order[1]
       board.reset_board
-      puts "#{firstp.name} goes first their mark is" + " #{firstp.player_mark}".colorize(:red)
-      puts "#{secondp.name} goes first their mark is" + " #{secondp.player_mark}".colorize(:blue)
+      puts "#{firstp.name} goes first their mark is" + " #{firstp.player_mark}"
+      puts "#{secondp.name} goes first their mark is" + " #{secondp.player_mark}"
       puts ""
       puts board.show_board
     else
-      puts "Thanks for playing, the final scores are #{firstp.show_player_id}:#{firstp.scores} and #{secondp.show_player_id}:#{secondp.scores}"
+      puts "Thanks for playing, the final scores are #{firstp.name}:#{firstp.player_points} and #{secondp.name}:#{secondp.player_points}"
       break
     end
   end
 
   puts "#{secondp.name} make your move"
-  move = gets.chomp
-  board.transform_board(move)
-  unless board.transform_board(move) == true
-    puts "Please try again"
-    move = gets.chomp
-    board.transform_board(move)
-  end
+  move = gets.chomp.to_i
+  board.finding(move)
+  board.transform_board(secondp.player_mark)
   board.show_transformed_board
 
   next unless board.horizontal_win? == true || board.vertical_win? == true ||
               board.down_left_diagonal_win? == true || board.down_right_diagonal_win?
 
   secondp.add_point
-  puts "#{secondp} congratulations for wining!"
+  puts "#{secondp.name} congratulations for wining!"
   winner = true
   puts "Would you like to keep playing Y/N?"
   answer = gets.chomp
@@ -181,13 +173,12 @@ until winner == true
     firstp = playing_order[0]
     secondp = playing_order[1]
     board.reset_board
-    puts "#{firstp.name} goes first their mark is" + " #{firstp.player_mark}".colorize(:red)
-    puts "#{secondp.name} goes first their mark is" + " #{secondp.player_mark}".colorize(:blue)
+    puts "#{firstp.name} goes first their mark is" + " #{firstp.player_mark}"
+    puts "#{secondp.name} goes first their mark is" + " #{secondp.player_mark}"
     puts ""
     puts board.show_board
   else
-    puts "Thanks for playing, the final scores are #{firstp.show_player_id}:#{firstp.scores} and #{secondp.show_player_id}:#{secondp.scores}"
+    puts "Thanks for playing, the final scores are #{firstp.name}:#{firstp.player_points} and #{secondp.name}:#{secondp.player_points}"
     break
   end
-
 end
