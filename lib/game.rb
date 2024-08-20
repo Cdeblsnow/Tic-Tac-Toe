b0d615f939cac3e8ed157c45f02caa1f90fa404c
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2024-08-20 00:12:34 -0400

    Add player win

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,5 @@
 require_relative "player"
+require_relative "board"
 class Game
   def self.shuffle_players(p1, p2)
     playing_order = [p1, p2]
@@ -20,5 +21,37 @@ class Game
 
   def self.player2
     @secondp
+  end
+
+  def self.p1_win?
+    if board.horizontal_win? == true || board.vertical_win? == true ||
+      board.down_left_diagonal_win? == true || board.down_right_diagonal_win?
+ 
+     Game.player1.add_point
+     return true
+     
+    end
+  end
+
+  def self.p2_win?
+    if board.horizontal_win? == true || board.vertical_win? == true ||
+      board.down_left_diagonal_win? == true || board.down_right_diagonal_win?
+ 
+     Game.player2.add_point
+     return true
+     
+    end
+  end
+
+  def self.new_game 
+       Game.player_creation
+       puts "#{Game.player1.name} goes first their mark is" + " #{Game.player1.player_mark}"
+       puts "#{Game.player2.name} goes first their mark is" + " #{Game.player2.player_mark}"
+       puts ""
+       puts Board.show_board
+  end
+
+  def self.reject_new_game
+    puts "Thanks for playing!, the final scores are #{Game.player1.name}:#{Game.player1.player_points} and #{Game.player2.name}:#{Game.player2.player_points}"
   end
 end
