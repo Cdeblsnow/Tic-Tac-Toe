43152fb023778faca15b0e52c2495848466221ae
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-03-20 12:25:52 -0400

    Feat: players can not override others marks

--- lib/board.rb
+++ lib/board.rb
@@ -1,50 +1,50 @@
 class Board
   attr_accessor :posx, :posy, :new_board
 
-
   def initialize
-    self.posx = 0
-    self.posy = 0
-    self.new_board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    @posx = 0
+    @posy = 0
+    @new_board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
   end
 
-  def self.show_board
-    new_board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+  def show_board
     3.times do |y|
-      3.times { |x| print new_board[x][y] } # 'vertical printing'
+      3.times { |x| print @new_board[x][y] } # 'vertical printing'
       puts
     end
   end
 
   def finding(move)
-    p move
-    p new_board
     3.times do |x|
       3.times do |y|
-        next unless new_board[x][y] == move # check for the index of the object if valid
+        next unless @new_board[x][y] == move # check for the index of the object if valid
 
-        self.posx = x
-        self.posy = y
+        @posx = x
+        @posy = y
         return
       end
     end
   end
 
   def reset_board
-    self.new_board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # reset and create the board we will be working with
+    @new_board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # reset and create the board we will be working with
   end
 
-  def transform_board(mark)
-    x = posx
-    y = posy
-    return false unless new_board[x][y] != "X" && new_board[x][y] != "O"
+  def transform_board(mark1, mark2, player_number)
+    x = @posx
+    y = @posy
+    return "Space has already been taken" unless @new_board[x][y] != mark1 && @new_board[x][y] != mark2
 
-    new_board[x][y] = mark
+    @new_board[x][y] = if player_number == 1
+                         mark1
+                       else
+                         mark2
+                       end
   end
 
   def show_transformed_board
     3.times do |y|
-      3.times { |x| print new_board[x][y] } # 'vertical printing'
+      3.times { |x| print @new_board[x][y] } # 'vertical printing'
       puts
     end
   end
@@ -52,7 +52,7 @@ class Board
   def horizontal_win?
     3.times do |row|
       2.times do |i|
-        break unless new_board[row][i] == new_board[row][i + 1]
+        break unless @new_board[row][i] == @new_board[row][i + 1]
         return true if i == 1
       end
     end
@@ -61,7 +61,7 @@ class Board
   def vertical_win?
     3.times do |column|
       2.times do |i|
-        break unless new_board[i][column] == new_board[i + 1][column]
+        break unless @new_board[i][column] == @new_board[i + 1][column]
         return true if i == 1
       end
     end
@@ -69,14 +69,14 @@ class Board
 
   def down_right_diagonal_win?
     2.times do |i|
-      break unless new_board[i][i] == new_board[i + 1][i + 1]
+      break unless @new_board[i][i] == @new_board[i + 1][i + 1]
       return true if i == 1
     end
   end
 
   def down_left_diagonal_win?
     2.times do |i|
-      break unless new_board[i][2 - i] == new_board[1 + i][1 - i]
+      break unless @new_board[i][2 - i] == @new_board[1 + i][1 - i]
       return true if i == 1
     end
   end
