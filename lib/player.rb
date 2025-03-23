f1da6e75d5b9cdeb27e11b18a9134e34c6082f88
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-03-23 00:13:46 -0400

    Remove shod id method

--- lib/player.rb
+++ lib/player.rb
@@ -1,22 +1,18 @@
 require "colorize"
 class Player
-  attr_accessor :player_mark, :player_points, :number_of_moves, :name
+  attr_reader :player_mark, :player_points, :name
 
   @@player_id = 0
 
   def initialize
     @@player_id += 1
-    self.name = "Player#{@@player_id}"
-    self.player_mark = (@@player_id == 1 ? "X".colorize(:red) : "O".colorize(:blue))
-    self.player_points = 0
-  end
-
-  def show_player_id
-    @@player_id
+    @name = "Player#{@@player_id}"
+    @player_mark = (@@player_id == 1 ? "X".colorize(:red) : "O".colorize(:blue))
+    @player_points = 0
   end
 
   def add_point
-    self.player_points += 1
+    @player_points += 1
   end
 
   def self.id_reset
