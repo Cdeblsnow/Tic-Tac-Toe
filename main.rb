7ebbf1491b3c2a626b8908e957fa7c163519e18d
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-03-26 18:52:04 -0400

    Update main and others

--- main.rb
+++ main.rb
@@ -4,7 +4,6 @@ require_relative "lib/game"
 
 board = Board.new
 game = Game.new
-game.player_creation
 winner = false
 players = [game.first_player, game.second_player]
 i = 0
@@ -19,23 +18,28 @@ until winner == true
   current_player = players[i % players.length]
 
   puts "#{current_player.name} make your move"
-  move = gets.chomp.to_i
-  if game.check_move(move) == true
+  move = gets.chomp
+  move = move[0].to_i
+  if game.check_move(move) == true && !move.between?(1, 9)
     puts "Please make a valid move"
     move = gets.chomp.to_i until game.check_move(move) == false
   end
   game.add_move(move)
   board.finding(move)
   board.transform_board(players[0].player_mark, players[1].player_mark, i)
-  board.show_transformed_board
 
-  if game.game_over(board)
+  if game.game_over(board, i)
     puts "#{current_player.name} congratulations for wining!"
     winner = true
     puts "Would you like to keep playing Y/N?"
     answer = gets.chomp.downcase
     case answer
-    when "y" then game.new_game && winner = false
+    when "y"
+      game.new_game
+      winner = false
+      game.reset_moves
+      board.reset_board
+      board.show_board
     when "n" then game.reject_new_game && break
     end
   end
