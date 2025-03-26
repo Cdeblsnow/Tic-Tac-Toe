e407e5008867353e7c75155b5e10cc80165824bf
Snow <151990949+Cdeblsnow@users.noreply.github.com>
2025-03-26 18:42:40 -0400

    Update player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,21 +1,14 @@
 require "colorize"
 class Player
-  attr_reader :player_mark, :player_points, :name
+  attr_reader :player_mark, :name
 
-  @@player_id = 0
-
-  def initialize
-    @@player_id += 1
-    @name = "Player#{@@player_id}"
-    @player_mark = (@@player_id == 1 ? "X".colorize(:red) : "O".colorize(:blue))
+  def initialize(id)
+    @name = "Player#{id}"
+    @player_mark = (id == 1 ? "X".colorize(:red) : "O".colorize(:blue))
     @player_points = 0
   end
 
   def add_point
     @player_points += 1
-  end
-
-  def self.id_reset
-    @@player_id = 0
   end
 end
